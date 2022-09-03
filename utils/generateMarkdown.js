6a38d23b7eee263c0450bc064e6c3eb63ae29449
Munnos <munnos@hotmail.co.uk>
2022-09-03 14:35:26 +0100

    changes to licences and need to troubleshoot

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  return `https://opensource.org/licenses/${license}`;
 
 }
 
