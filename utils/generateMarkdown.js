e0e7aa679a04657163351ac0df09776b9be34cd1
Munnos <munnos@hotmail.co.uk>
2022-09-03 14:33:10 +0100

    changes to generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,6 +18,59 @@ function renderLicenseLink(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+
+## Description
+
+ ${data.description}
+
+ 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Screenshot](${data.screenshot})
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Screenshot
+
+![Screenshot](${data.screenshot})
+
+## License
+
+${renderLicenseLink(data.license)}
+
+This project is under the license of ${data.license}
+
+## Contributing 
+
+${data.contributors}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+If you have any questions about this project, you can contact me at ${
+  data.email
+}. More projects available here https://github.com/${data.github}.
+`;
+  
 }
 
 
